39bf477b521ac4d5b3fd6793e2c21c87725268f7
Ramkrishnank <40989028+Ramkrishnank@users.noreply.github.com>
2019-06-09 22:48:58 +0530

    Branch created

---
+++ 3DObject_Exporter/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using HelixToolkit.Wpf;
+using MaterialDesignColors.WpfExample.Domain;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3DObject_Exporter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Snackbar Snackbar;
+        public MainWindow()
+        {
+            InitializeComponent();
+            view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2500);
+            }).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue.Enqueue("Welcome to 3D Object Exchanger Tookit");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+           // DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue);
+
+            Snackbar = this.MainSnackbar;
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private async void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //var sampleMessageDialog = new SampleMessageDialog
+            //{
+            //    Message = { Text = ((ButtonBase)sender).Content.ToString() }
+            //};
+
+           // await DialogHost.Show(sampleMessageDialog, "RootDialog");
+        }
+
+        private void OnCopy(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Parameter is string stringValue)
+            {
+                try
+                {
+                    Clipboard.SetDataObject(stringValue);
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.ToString());
+                }
+            }
+        }
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            //pgBar.IsEnabled=true;
+            //pgBar.Value = 90;
+           SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Exported Model";
+           if(saveFileDialog.ShowDialog() == true)
+            {
+                saveFileDialog.CheckFileExists = true;
+                saveFileDialog.AddExtension = true;
+                saveFileDialog.FileName = view.GetName();
+                saveFileDialog.Filter = "Object files (*.STL)|*.STL|All files (*.*)|*.*";
+                // saveFileDialog.FileOk(sender);
+            }
+
+        }
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+
+            openFileDialog.Filter = "Object files (*.STL,*.OBJ,*.FBX,*.3DS)|*.STL,*.OBJ,*.FBX,*.3DS|All files (*.*)|*.*";
+           
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //Get the path of specified file
+                var filePath = openFileDialog.FileName;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+                var fileContent = "";
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                    ModelVisual3D device3D = new ModelVisual3D();
+                    device3D.Content = Display3D(filePath);
+                    view.Children.Add(device3D);
+                }
+                //view.Export(filePath);
+              
+            }
+        }
+        private Model3D Display3D(string ModelPath)
+        {
+            Model3D Device = null;
+            try
+            {
+                view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+                ModelImporter import = new ModelImporter();
+                Device = import.Load(ModelPath);
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show("Exception:" + E.StackTrace);
+            }
+            return Device;
+        }
+    }
+}
---
+++ 3DObject_Exporter/ModelCreation.cs
@@ -0,0 +1,110 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace _3DObject_Exporter
+{
+   public class ModelCreation : ParametricSurface3D
+    {
+        /// <summary>
+        /// Identifies the <see cref="B"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty BProperty =
+            DependencyProperty.Register("B", typeof(double), typeof(ModelCreation), new PropertyMetadata(3d, GeometryChanged));
+
+        /// <summary>
+        /// Identifies the <see cref="C1"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty C1Property =
+            DependencyProperty.Register("C1", typeof(double), typeof(ModelCreation), new PropertyMetadata(10d, GeometryChanged));
+
+        /// <summary>
+        /// Identifies the <see cref="C2"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty C2Property =
+            DependencyProperty.Register("C2", typeof(double), typeof(ModelCreation), new PropertyMetadata(2d, GeometryChanged));
+
+        /// <summary>
+        /// Gets or sets the b parameter. The default is 3.
+        /// </summary>
+        /// <value>The b value.</value>
+        public double B
+        {
+            get { return (double)this.GetValue(BProperty); }
+            set { this.SetValue(BProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the c1 parameter. The default is 10.
+        /// </summary>
+        /// <value>The c1 value.</value>
+        public double C1
+        {
+            get { return (double)this.GetValue(C1Property); }
+            set { this.SetValue(C1Property, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the c2 parameter. The default is 2.
+        /// </summary>
+        /// <value>The c2 value.</value>
+        public double C2
+        {
+            get { return (double)this.GetValue(C2Property); }
+            set { this.SetValue(C2Property, value); }
+        }
+
+        /// <summary>
+        /// Evaluates the surface at the specified u,v parameters.
+        /// </summary>
+        /// <param name="u">The u parameter.</param>
+        /// <param name="v">The v parameter.</param>
+        /// <param name="uv">The texture coordinates.</param>
+        /// <returns>The evaluated <see cref="T:System.Windows.Media.Media3D.Point3D" />.</returns>
+        protected override Point3D Evaluate(double u, double v, out Point uv)
+        {
+            const double pi = Math.PI;
+            u *= 2 * pi;
+            v = (v - 0.5) * 2 * pi;
+
+            double cosv = Math.Cos(v);
+            double sinv = Math.Sin(v);
+
+            double x = Math.Cos(u) * (4 + 3.8 * cosv);
+            double y = Math.Sin(u) * (4 + 3.8 * cosv);
+            double z = (cosv + sinv - 1) * (1 + sinv) * Math.Log(1 - pi * v / 10) + 7.5 * sinv;
+            uv = new Point(u, 0);
+            return new Point3D(x, y, z);
+            //u *= 4 * Math.PI;
+            //v = (v - 0.5) * 2 * Math.PI;
+
+            //double b = this.B;
+
+            //double cosu = Math.Cos(u);
+            //double sinu = Math.Sin(u);
+            //double cosv = Math.Cos(v);
+            //double sinv = Math.Sin(v);
+
+            //double c1 = this.C1;
+            //double c2 = this.C2;
+            //double c3 = c1 * (Math.Cos(b * u / 2) + b) / 4;
+            //double c4 = -c3 * sinu - b * c1 * Math.Sin(b * u / 2) * cosu / 8;
+            //double c5 = c3 * cosu - b * c1 * Math.Sin(b * u / 2) * sinu / 8;
+            //double c6 = (b * c3 * Math.Cos(Math.Sin(b * u / 2)) * Math.Cos(b * u / 2)) / 2 - (b * c1 * Math.Sin(Math.Sin(b * u / 2)) * Math.Sin(b * u / 2)) / 8;
+            //double c7 = Math.Sqrt(c4 * c4 + c5 * c5);
+            //double c8 = Math.Sqrt(c4 * c4 + c5 * c5 + c6 * c6);
+
+            //var x = c3 * cosu + (c2 * (c8 * cosv * c5 - sinv * c4 * c6) / (c7 * c8));
+            //var y = c3 * sinu - (c2 * (c8 * cosv * c4 + sinv * c5 * c6) / (c7 * c8));
+            //var z = c3 * Math.Sin(Math.Sin(b * u / 2)) + (c2 * sinv * c7 / c8);
+
+            //uv = new Point(u, v);
+            //return new Point3D(x, y, z);
+        }
+    }
+}
