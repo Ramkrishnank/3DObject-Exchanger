997ebdc196b40d772193fd8d0292b795c4a37a39
Ramk <prabhuraju06@gmail.com>
2019-06-14 22:24:38 +0530

    Majar conversion completed

--- 3DObject_Exporter/MainWindow.xaml.cs
+++ 3DObject_Exporter/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using HelixToolkit.Wpf;
+﻿using devDept.Eyeshot;
+using devDept.Eyeshot.Entities;
+using devDept.Eyeshot.Translators;
+using HelixToolkit.Wpf;
 using MaterialDesignColors.WpfExample.Domain;
 using MaterialDesignThemes.Wpf;
 using Microsoft.Win32;
@@ -29,11 +32,12 @@ namespace _3DObject_Exporter
     /// </summary>
     public partial class MainWindow : Window
     {
+       // private static readonly Color NOT_MODIFIED_COLOR = Color.FromArgb(44, 44, 44);
         public static Snackbar Snackbar;
         public MainWindow()
         {
             InitializeComponent();
-            view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+            //view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
             Task.Factory.StartNew(() =>
             {
                 Thread.Sleep(2500);
@@ -89,25 +93,49 @@ namespace _3DObject_Exporter
         private void btnExport_Click(object sender, RoutedEventArgs e)
         {
             //pgBar.IsEnabled=true;
-            //pgBar.Value = 90;
+            //pgBar.Value = 90;    
+          
+            string objFile = "Test.Step";
            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            var writeSTL = new WriteSTEP(new WriteParamsWithUnits(model1), objFile);
+            var RS = new WriteParamsWithMaterials(model1);
+
+            writeSTL.DoWork();
             saveFileDialog.Title = "Exported Model";
            if(saveFileDialog.ShowDialog() == true)
             {
                 saveFileDialog.CheckFileExists = true;
                 saveFileDialog.AddExtension = true;
-                saveFileDialog.FileName = view.GetName();
+                saveFileDialog.FileName = writeSTL.ToString();
                 saveFileDialog.Filter = "Object files (*.STL)|*.STL|All files (*.*)|*.*";
-                // saveFileDialog.FileOk(sender);
+                //saveFileDialog.FileOk(writeSTL);
             }
+
+            string stlFile = "chair.stl";
+            WriteSTL ws = new WriteSTL(new WriteParams(model1), stlFile, true);
+
+            ws.DoWork();
+
+            string fullPath = String.Format(@"{0}\{1}", System.Environment.CurrentDirectory, stlFile);
+            MessageBox.Show(String.Format("File saved in {0}", fullPath));
+            WriteSTEP wrStep = new WriteSTEP(new WriteParamsWithUnits(model1), "wing.stp");
+            wrStep.DoWork();
+            MessageBox.Show("File 'wing.stp' successfully written.");
 
         }
         private void btnImport_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "STL File|*.STL",
+                Multiselect = false,
+                AddExtension = true,
+                CheckFileExists = true,
+                CheckPathExists = true
+            };
             openFileDialog.InitialDirectory = "c:\\";
 
-            openFileDialog.Filter = "Object files (*.STL,*.OBJ,*.FBX,*.3DS)|*.STL,*.OBJ,*.FBX,*.3DS|All files (*.*)|*.*";
+          //  openFileDialog.Filter = "Object files (*.STL,*.OBJ,*.FBX,*.3DS)|*.STL,*.OBJ,*.FBX,*.3DS|All files (*.*)|*.*";
            
             if (openFileDialog.ShowDialog() == true)
             {
@@ -119,29 +147,88 @@ namespace _3DObject_Exporter
                 var fileContent = "";
                 using (StreamReader reader = new StreamReader(fileStream))
                 {
-                    fileContent = reader.ReadToEnd();
-                    ModelVisual3D device3D = new ModelVisual3D();
-                    device3D.Content = Display3D(filePath);
-                    view.Children.Add(device3D);
+                    //devDept.Eyeshot.Translators.ReadFile filePath = new devDept.Eyeshot.Translators.ReadFile(filePath);
+                    model1.Clear();
+                    var readAutodesk = new ReadAutodesk (filePath);
+                    readAutodesk.DoWork();
+                    readAutodesk.AddToScene(model1);
+                    var ReadObj = new ReadSTL(filePath);
+                    ReadObj.DoWork();
+                    Entity[] toAdd = ReadObj.Entities;
+                    
+                    //fileContent = reader.ReadToEnd();
+                    //ModelVisual3D device3D = new ModelVisual3D();
+                    //Viewport3DVisual viewport = new Viewport3DVisual();
+                    //viewport readFile;
+                   
+                    
+                    model1.Entities.AddRange(toAdd, System.Drawing.Color.Chocolate);
+                    //model1.SetView(viewType.Top);
+                    //model1.ZoomFit();
+                    model1.Invalidate();
                 }
                 //view.Export(filePath);
               
             }
         }
-        private Model3D Display3D(string ModelPath)
-        {
-            Model3D Device = null;
-            try
-            {
-                view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
-                ModelImporter import = new ModelImporter();
-                Device = import.Load(ModelPath);
-            }
-            catch (Exception E)
-            {
-                MessageBox.Show("Exception:" + E.StackTrace);
-            }
-            return Device;
-        }
+        //private Model3D Display3D(string ModelPath)
+        //{
+        //    Model3D Device = null;
+        //    try
+        //    {
+        //        view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+        //        ModelImporter import = new ModelImporter();
+        //        Device = import.Load(ModelPath);
+        //    }
+        //    catch (Exception E)
+        //    {
+        //        MessageBox.Show("Exception:" + E.StackTrace);
+        //    }
+        //    return Device;
+        //}
+
+        //protected override void OnContentRendered(EventArgs e)
+        //{
+        //    model1.GetGrid().AutoSize = true;
+        //    model1.GetGrid().Step = 500;
+
+        //    devDept.Eyeshot.Translators.ReadFile readFile = new devDept.Eyeshot.Translators.ReadFile(Assets + "160.eye");
+        //    readFile.DoWork();
+        //    model1.Entities.AddRange(readFile.Entities, System.Drawing.Color.DimGray);
+
+        //    Block firstBlock = new Block("First");
+        //    AddStlToBlock(firstBlock, "930.eye", System.Drawing.Color.DeepSkyBlue);
+        //    AddStlToBlock(firstBlock, "940.eye", System.Drawing.Color.DeepSkyBlue);
+
+        //    Block secondBlock = new Block("Second");
+        //    AddStlToBlock(secondBlock, "570.eye", System.Drawing.Color.DodgerBlue);
+
+        //    Block thirdBlock = new Block("Third");
+        //    AddStlToBlock(thirdBlock, "590.eye", System.Drawing.Color.SlateBlue);
+
+        //    firstBlock.Entities.Add(new TranslatingAlongY("Second"));
+        //    secondBlock.Entities.Add(new TranslatingAlongZ("Third"));
+
+        //    model1.Blocks.Add(firstBlock);
+        //    model1.Blocks.Add(secondBlock);
+        //    model1.Blocks.Add(thirdBlock);
+
+        //    model1.Entities.Add(new TranslatingAlongX("First"));
+
+        //    model1.SetView(viewType.Trimetric);
+
+        //    model1.ZoomFit();
+
+        //    // Turn off silhouettes to increase drawing speed
+        //    model1.Rendered.SilhouettesDrawingMode = silhouettesDrawingType.Never;
+
+        //    // Shadows are not currently supported in animations
+        //    model1.Rendered.ShadowMode = shadowType.None;
+
+        //    model1.StartAnimation(_interval);
+        //    startButton.IsEnabled = false;
+
+        //    base.OnContentRendered(e);
+        //}
     }
 }
